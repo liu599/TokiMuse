a92315d66b93920d9b30e6e70c8a456947d36d0b
liu599 <970228409@qq.com>
2017-07-14 23:32:13 +0800

    Finish music-list

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="anticon icon-left"></i>
     </div>
     <h1 class="title" v-html="title"></h1>
     <div class="bg-image" :style="bgStyle" ref="bgImage">
+      <div v-show="songs.length>0" ref="playBtn" class="play-wrapper">
+        <div class="play">
+          <i class="anticon icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
       <div class="filter" ref="filter"></div>
     </div>
     <div class="bg-layer" ref="layer"></div>
@@ -14,15 +20,22 @@
       <div class="song-list-wrapper">
         <song-list :songs="songs"></song-list>
       </div>
+      <div v-show="!songs.length" class="loading-container">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 <script type="text/ecmascript-6">
   import Scroll from 'base/scroll/scroll'
-  import SongList from 'base/song-list/song-list';
+  import Loading from 'base/loading/loading'
+  import SongList from 'base/song-list/song-list'
+  import { prefixStyle } from 'common/js/dom'
 
   const RESERVED_HEIGHT = 40
-
+  const transform = prefixStyle('transform')
+  const backdrop = prefixStyle('backdrop-filter')
+  // console.log(backdrop)
   export default {
     props: {
       bgImage: {
@@ -51,6 +64,9 @@
     methods: {
       scroll(pos) {
         this.scrollY = pos.y
+      },
+      back() {
+        this.$router.back()
       }
     },
     watch: {
@@ -67,23 +83,22 @@
         } else {
           blur = Math.min(20 * percent, 20)
         }
-        this.$refs.filter.style['backdrop-filter'] = `blur(${blur}px)`
-        this.$refs.filter.style['webkitBackdrop-filter'] = `blur(${blur}px)`
-        this.$refs.layer.style['transform'] = `translate3d(0, ${translateY}px, 0)`
-        this.$refs.layer.style['webkitTransform'] = `translate3d(0, ${translateY}px, 0)`
+        this.$refs.filter.style[backdrop] = `blur(${blur}px)`
+        this.$refs.layer.style[transform] = `translate3d(0, ${translateY}px, 0)`
         // 改变图片的大小
 
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
           this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         } else {
           this.$refs.bgImage.style.paddingTop = `70%`
           this.$refs.bgImage.style.height = 0
+          this.$refs.playBtn.style.display = ''
         }
         this.$refs.bgImage.style.zIndex = zIndex
-        this.$refs.bgImage.style['transform'] = `scale(${scale})`
-        this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
+        this.$refs.bgImage.style[transform] = `scale(${scale})`
       }
     },
     created() {
@@ -98,7 +113,8 @@
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     }
   }
 </script>
