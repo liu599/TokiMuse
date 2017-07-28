abf62c0f9df54c1e23344a22f4ca99f7e74e22b6
liu599 <970228409@qq.com>
2017-07-29 00:29:41 +0800

    Finish transition Animation

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,72 +1,84 @@
 <template>
   <div class="player" v-show="playlist.length>0">
-    <div class="normal-player" v-show="fullScreen">
-      <div class="background">
-        <img width="100%" height="100%" :src="currentSong.image">
-      </div>
-      <div class="top">
-        <div class="back" @click="back">
-          <i class="anticon icon-downcircleo"></i>
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave">
+      <div class="normal-player" v-show="fullScreen">
+        <div class="background">
+          <img width="100%" height="100%" :src="currentSong.image">
         </div>
-        <h1 class="title" v-html="currentSong.name"></h1>
-        <h2 class="subtitle" v-html="currentSong.singer"></h2>
-      </div>
-      <div class="middle">
-        <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
-              <img class="image" :src="currentSong.image">
+        <div class="top">
+          <div class="back" @click="back">
+            <i class="anticon icon-left"></i>
+          </div>
+          <h1 class="title" v-html="currentSong.name"></h1>
+          <h2 class="subtitle" v-html="currentSong.singer"></h2>
+        </div>
+        <div class="middle">
+          <div class="middle-l">
+            <div class="cd-wrapper" ref="cdWrapper">
+              <div class="cd">
+                <img class="image" :src="currentSong.image">
+              </div>
+            </div>
+            <div class="playing-lyric-wrapper">
+              <div class="playing-lyric"></div>
             </div>
           </div>
-          <div class="playing-lyric-wrapper">
-            <div class="playing-lyric"></div>
+        </div>
+        <div class="bottom">
+          <div class="dotwrapper"></div>
+          <div class="progress-wrapper">
+            <div class="progress-bar-wrapper"></div>
+            <span class="time time-r"></span>
+          </div>
+          <div class="operators">
+            <div class="icon i-left">
+              <i class="anticon icon-retweet"></i>
+            </div>
+            <div class="icon i-left">
+              <i class="anticon icon-stepbackward"></i>
+            </div>
+            <div class="icon i-center">
+              <i class="anticon icon-play"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="anticon icon-stepforward"></i>
+            </div>
+            <div class="icon i-right">
+              <i class="anticon icon-unlock"></i>
+            </div>
           </div>
         </div>
       </div>
-      <div class="bottom">
-        <div class="dotwrapper"></div>
-        <div class="progress-wrapper">
-          <div class="progress-bar-wrapper"></div>
-          <span class="time time-r"></span>
+    </transition>
+    <transition name="mini">
+      <div class="mini-player" v-show="!fullScreen" @click="open">
+        <div class="icon">
+          <img width="40" height="40" :src="currentSong.image">
         </div>
-        <div class="operators">
-          <div class="icon i-left">
-            <i></i>
-          </div>
-          <div class="icon i-left">
-            <i class="anticon icon-stepbackward"></i>
-          </div>
-          <div class="icon i-center">
-            <i class="anticon icon-play"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="anticon icon-stepforward"></i>
-          </div>
-          <div class="icon i-right">
-            <i class="anticon icon-unlock"></i>
-          </div>
+        <div class="text">
+          <h2 class="name" v-html="currentSong.name"></h2>
+          <p class="desc" v-html="currentSong.singer"></p>
+        </div>
+        <div class="control"></div>
+        <div class="control">
+          <i class="anticon icon-menufold"></i>
         </div>
       </div>
-    </div>
-    <div class="mini-player" v-show="!fullScreen" @click="open">
-      <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
-      </div>
-      <div class="text">
-        <h2 class="name" v-html="currentSong.name"></h2>
-        <p class="desc" v-html="currentSong.singer"></p>
-      </div>
-      <div class="control"></div>
-      <div class="control">
-        <i class="anticon icon-menufold"></i>
-      </div>
-    </div>
+    </transition>
+
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   import { mapGetters, mapMutations } from 'vuex'
+  import animations from 'create-keyframe-animation'
+  import {prefixStyle} from 'common/js/dom'
 
+  const transform = prefixStyle('transform')
   export default {
     computed: {
       ...mapGetters([
@@ -83,6 +95,63 @@
       },
       open() {
         this.setFullScreen(true)
+      },
+      enter(el, done) {
+        // 使用create-keyframe-animation
+        const {x, y, scale} = this._getPosAndScale()
+
+        let animation = {
+          0: {
+            transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+          },
+          60: {
+            transform: `translate3d(0,0,0) scale(1.1)`
+          },
+          100: {
+            transform: `translate3d(0,0,0) scale(1)`
+          }
+        }
+
+        animations.registerAnimation({
+          name: 'move',
+          animation,
+          presets: {
+            duration: 400,
+            easing: 'linear'
+          }
+        })
+        // 使用DOM
+        animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+      afterEnter () {
+        animations.unregisterAnimation('move')
+        this.$refs.cdWrapper.style.animation = ''
+      },
+      leave(el, done) {
+        this.$refs.cdWrapper.style.transition = 'all 0.4s'
+        const {x, y, scale} = this._getPosAndScale()
+        this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+        this.$refs.cdWrapper.addEventListener('transitionend', done)
+      },
+      afterLeave() {
+        this.$refs.cdWrapper.style.transition = ''
+        this.$refs.cdWrapper.style[transform] = ''
+      },
+      _getPosAndScale() {
+        // 获取初始的位置
+        const targetWidth = 40
+        const paddingLeft = 40
+        const paddingBottom = 30
+        const paddingTop = 80
+        const width = window.innerWidth * 0.8
+        const scale = targetWidth / width
+        const x = -(window.innerWidth / 2 - paddingLeft)
+        const y = window.innerHeight - paddingTop - width / 2 - paddingBottom
+        return {
+          x,
+          y,
+          scale
+        }
       },
       ...mapMutations({
         setFullScreen: 'SET_FULL_SCREEN'
@@ -121,12 +190,12 @@
           top: 0
           left: 6px
           z-index: 50
-          .icon-downcircleo
+          .icon-left
             display: block
             padding: 9px
             font-size: $font-size-large-x
             color: deeppink
-            transform: rotate(0deg)
+            transform: rotate(-90deg)
         .title
           width: 70%
           margin: 0 auto
